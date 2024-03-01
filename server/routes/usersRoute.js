f32a8f9fa504e880753329355bb5222b1d22d912
Ludovil <ludovic_saule@hotmail.fr>
2024-03-01 12:17:09 +0100

    set up usersRoute

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -1,4 +1,27 @@
 import express from "express";
-import { createUser } from "../controllers/usersController";
+import {
+  createUser,
+  getUser,
+  loginUser,
+  updateUser,
+  deleteUser,
+} from "../controllers/usersController.js";
 
 const router = express.Router();
+
+// thunderclient : post http://localhost:2000/users
+router.post("/", createUser);
+
+// thunderclient : post http://localhost:2000/login
+router.post("/login", loginUser);
+
+// thunderclient : get http://localhost:2000/users/id
+router.get("/:id", getUser);
+
+// thunderclient : put http://localhost:2000/users/id
+router.put("/:id", updateUser);
+
+// thunderclient : delete http://localhost:2000/users/id
+router.delete("/:id", deleteUser);
+
+export default router;
